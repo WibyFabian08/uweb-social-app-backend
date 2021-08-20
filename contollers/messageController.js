f64d552f9582a68adbd919570d91b39f7ab1a164
WibyFabian08 <wibyfabian08@gmail.com>
2021-08-20 12:45:46 -0700

    error handler

--- contollers/messageController.js
+++ contollers/messageController.js
@@ -2,48 +2,62 @@ const Message = require("../models/Message");
 const Conversation = require("../models/Conversation");
 
 exports.createMessage = async (req, res) => {
-  const conversation = await Conversation.findOne({
-    _id: req.body.conversationId,
-  });
+  try {
+    const conversation = await Conversation.findOne({
+      _id: req.body.conversationId,
+    });
 
-  if (!conversation) {
-    return res.status(404).json({
+    if (!conversation) {
+      return res.status(404).json({
+        status: "error",
+        message: "conversation not found",
+      });
+    }
+
+    const message = new Message({
+      conversationId: conversation._id,
+      senderId: req.body.senderId,
+      text: req.body.text,
+    });
+
+    await message.save();
+
+    return res.status(200).json({
+      status: "ok",
+      message,
+    });
+  } catch (err) {
+    return res.status(500).json({
       status: "error",
-      message: "conversation not found",
+      message: "cannot create message",
     });
   }
-
-  const message = new Message({
-    conversationId: conversation._id,
-    senderId: req.body.senderId,
-    text: req.body.text,
-  });
-
-  await message.save();
-
-  return res.status(200).json({
-    status: "ok",
-    message,
-  });
 };
 
 exports.getMessage = async (req, res) => {
-  const conversation = await Conversation.findOne({
-    _id: req.params.conversationId,
-  });
+  try {
+    const conversation = await Conversation.findOne({
+      _id: req.params.conversationId,
+    });
 
-  if (!conversation) {
-    return res.status(404).json({
+    if (!conversation) {
+      return res.status(404).json({
+        status: "error",
+        message: "conversation not found",
+      });
+    }
+
+    const messages = await Message.find({ conversationId: conversation._id });
+
+    return res.status(200).json({
+      status: "ok",
+      message: "get message success",
+      messages,
+    });
+  } catch (err) {
+    return res.status(500).json({
       status: "error",
-      message: "conversation not found",
+      message: "get message failed",
     });
   }
-
-  const messages = await Message.find({conversationId: conversation._id})
-
-  return res.status(200).json({
-    status: "ok",
-    message: "get message success",
-    messages,
-  });
 };
